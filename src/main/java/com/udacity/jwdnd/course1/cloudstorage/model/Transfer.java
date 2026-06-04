269b091fb90fe11f7aad52ed6b7e1d73d653be7d
Jayanthi Dorepalli <jayanthidorepalli@gmail.com>
2026-06-04 11:39:16 +0530

    Added Transfer model for MFT platform

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Transfer.java
@@ -0,0 +1,100 @@
+package com.udacity.jwdnd.course1.cloudstorage.model;
+
+public class Transfer {
+
+    private Long transferId;
+    private Long partnerId;
+    private Long uploadedBy;
+
+    private String fileName;
+    private String fileType;
+    private Long fileSize;
+
+    private String status;
+    private String s3Key;
+    private String uploadTime;
+    private String validationMessage;
+
+    public Transfer() {
+    }
+
+    public Long getTransferId() {
+        return transferId;
+    }
+
+    public void setTransferId(Long transferId) {
+        this.transferId = transferId;
+    }
+
+    public Long getPartnerId() {
+        return partnerId;
+    }
+
+    public void setPartnerId(Long partnerId) {
+        this.partnerId = partnerId;
+    }
+
+    public Long getUploadedBy() {
+        return uploadedBy;
+    }
+
+    public void setUploadedBy(Long uploadedBy) {
+        this.uploadedBy = uploadedBy;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public Long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(Long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getS3Key() {
+        return s3Key;
+    }
+
+    public void setS3Key(String s3Key) {
+        this.s3Key = s3Key;
+    }
+
+    public String getUploadTime() {
+        return uploadTime;
+    }
+
+    public void setUploadTime(String uploadTime) {
+        this.uploadTime = uploadTime;
+    }
+
+    public String getValidationMessage() {
+        return validationMessage;
+    }
+
+    public void setValidationMessage(String validationMessage) {
+        this.validationMessage = validationMessage;
+    }
+}
